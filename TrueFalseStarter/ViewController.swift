4863e8c2944f5e9edafd5e3a4e6ef47d1ecd59e3
Cori Mims <cdmims21@gmail.com>
2016-11-30 16:13:59 -0500

    Added buttons and new layout.

--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -30,6 +30,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var questionThree: UIButton!
+    @IBOutlet weak var questionFour: UIButton!
     
 
     override func viewDidLoad() {
